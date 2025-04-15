500f9878e0a0221d55a8b16da3763abc28a6c202
JJMutu <jaredmutu@hotmail.com>
2025-04-15 20:56:26 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# RiskMate Demo
+
+## Setup
+
+1. Upload this folder to a GitHub repo
+2. Link it to [Vercel](https://vercel.com/)
+3. Deploy
+
+You're now previewing the RiskMate safety app.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+// RiskMate Demo
+import React from 'react';
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>RiskMate Demo</h1>
+      <p>Welcome Jared Mutu</p>
+      <p>SWMS Selector Coming Soon...</p>
+    </div>
+  );
+}
+export default App;
